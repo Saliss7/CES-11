15f8d755e6ed0b3fddea30106f3fa24fb0ce5ee5
MatheusFelipe <matheusfeliperb.mf@gmail.com>
2024-08-23 19:24:12 -0300

    Lab 1: reading variables

--- Lab 1 - Torre de controle/main.cpp
+++ Lab 1 - Torre de controle/main.cpp
@@ -4,24 +4,52 @@
 /*                                    */
 /* Programa compilado com CLion 17.12 */
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct{
+typedef struct aviao noh;
+
+struct aviao{
+    char menssagem[70];
     int numero;
-    char localPartida;
-    int* proximoFila;
-}aviao;
+    char localPartida[30];
+    noh* proximoFila = NULL;
+};
 
 int main() {
 
-    int* primeiroFila = NULL;
-    aviao aviao[100];
+    noh* primeiroFila = NULL;
+    noh* p = NULL;
+    int i = 0;
+    bool lerArquivo = true;
+    char linha[200];
     FILE* Entrada;
     Entrada = fopen("entrada1.txt", "r");
 
-    if (Entrada == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        return 0;
+    //pular as 8 primeiras linhas do arquivo de entrada
+    for (int i = 0; i < 8; i++) {
+        fgets(linha, sizeof(linha), Entrada);
+    }
+
+    while(lerArquivo) {
+        if(primeiroFila == NULL) {
+            primeiroFila = (noh*)malloc(sizeof(noh));
+            fscanf(Entrada, "%s", &primeiroFila->menssagem);
+            fscanf(Entrada, "%d ", &primeiroFila->numero);
+            fgets(primeiroFila->localPartida, Entrada);
+            primeiroFila->proximoFila = NULL;
+        } else {
+            primeiroFila = (noh*)malloc(sizeof(noh));
+            fscanf(Entrada, "%s", &p->menssagem);
+            fscanf(Entrada, "%d ", &p->numero);
+            fgets(p->localPartida, Entrada);
+            p->proximoFila = primeiroFila;
+            while (p->proximoFila != NULL) p->proximoFila = p->proximoFila->proximoFila;
+        }
+
+
+        i++;
     }
 
     fclose(Entrada);
